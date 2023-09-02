214710b39b187e76d78269427e85522de2535c82
Hittitech <mdadnansami9381@gmail.com>
2023-09-03 02:17:58 +0530

    added socials

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,28 +14,28 @@ export const Footer = () => {
           </div>
           <div className='footer__contact'>
             <li className='footer__list'>
-              <i className='footer__icon fas fa-home'></i>Mumbai, Maharashtra
+              <i className='footer__icon fas fa-home'></i>Hyderabad, Telangana
             </li>
             <li className='footer__list'>
               <i className='footer__icon fas fa-envelope'></i>
-              eaglestores@gmail.com
+              eaglestores123@gmail.com
             </li>
           </div>
           <div className='footer__social'>
             <a
-              href='https://github.com/anuj-kumary/Eagle-stores'
+              href='https://github.com/Hittitech/Eagle-store'
               className='footer__list'
             >
               <i className='footer__icon fab fa-github'></i>
             </a>
             <a
-              href='https://www.linkedin.com/in/anujkumar-yadav-29b2521aa/'
+              href='https://www.linkedin.com/in/md-adnan-sami/'
               className='footer__list '
             >
               <i class='footer__icon fab fa-linkedin'></i>
             </a>
             <a
-              href='https://twitter.com/TheRealAnujK'
+              href='https://twitter.com/adnan9381'
               className='footer__list '
             >
               <i className='footer__icon fab fa-twitter'></i>
